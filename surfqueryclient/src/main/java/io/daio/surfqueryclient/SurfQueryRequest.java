01b5d1b7eef1ef781930be8cb53da93c78b5dbf7
Daio-io <dai.williams@bbc.co.uk>
2015-12-29 13:36:58 +0000

    Add constants for request parameter keys

--- surfqueryclient/src/main/java/io/daio/surfqueryclient/SurfQueryRequest.java
+++ surfqueryclient/src/main/java/io/daio/surfqueryclient/SurfQueryRequest.java
@@ -7,6 +7,43 @@ import java.util.HashMap;
 
 public final class SurfQueryRequest {
 
+    /**
+     * Parameter key to set the spot / beach id to submit the query against
+     *
+     * e.g. spotid=1449 will return results for Rest Bay
+     */
+    public static final String SPOT_ID_PARAM = "spotid";
+
+    /**
+     * Parameter key to set the maximum swell range in results.
+     *
+     * e.g. maxswell = 10 will only return results with a swell up to and inclusive of 10
+     */
+    public static final String MAX_SWELL_PARAM = "maxswell";
+
+    /**
+     * Parameter key to set the minimum swell range in results.
+     *
+     * e.g. minswell=2 will only return results with a swell above and inclusive of 2
+     */
+    public static final String MIN_SWELL_PARAM = "minswell";
+
+    /**
+     * Set the start time you want results from
+     *
+     * e.g. setting start=8 will only return results starting from 8am on that day.
+     * Clock is 24 hour in the query
+     */
+    public static final String START_TIME_PARAM = "start";
+
+    /**
+     * Set the end time you want results from
+     *
+     * e.g. setting end=22 will only return results up to 10pm on that day.
+     * Clock is 24 hour in the query
+     */
+    public static final String END_TIME_PARAM = "end";
+
     private HashMap<String, String> params;
 
     public SurfQueryRequest() {
@@ -21,6 +58,5 @@ public final class SurfQueryRequest {
     public HashMap<String, String> getParams() {
         return this.params;
     }
-
 
 }
